012de6aded30770c9d96496c69b8dfa8af5a190c
brettfeltmate <the.bretter.email@gmail.com>
2019-03-01 15:37:28 -0400

    Fixed timing issues.

--- experiment.py
+++ experiment.py
@@ -238,7 +238,7 @@ class SSAT_line(klibs.Experiment):
 			if len(self.temporal_pre_rc.keypress_listener.responses):
 				temporal_response, temporal_rt = self.temporal_pre_rc.keypress_listener.response()
 
-			self.stream_sw.stop()
+			self.stream_sw.pause()
 
 			else:
 				self.temporal_post_rc.collect()
